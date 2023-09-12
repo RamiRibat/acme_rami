edbcbd08122ef0676d2d3e3522c9e6d4ac995836
RamiSketcher <ramisketcher@gmail.com>
2023-09-12 06:01:15 -0400

    update sh

--- next/rl_continuous/script.sh
+++ next/rl_continuous/script.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+TASKS=(
+    "cartplole_swingup" "walker_walk"
+    "hopper_stand" "swimmer_swimmer6"
+    "cheetah_run" "walker_run"
+)
+
+SEEDS=(1 2 3)
+
+
 DATETIME=$(date +'%Y%m%d-%H%M%S')
 
 ID="$DATETIME"
 
-python run_d4pg_2.py --acme_id $ID --seed 1
-python run_d4pg_2.py --acme_id $ID --seed 2
-python run_d4pg_2.py --acme_id $ID --seed 3
+
+for T in ${TASKS[*]}
+do
+    ENVID="control_$T"
+    for S in ${SEEDS[*]}
+    do
+        echo $ENVID $S
+        python run_d4pg_2.py --acme_id $ID --env_name $ENVID --seed $S
+    done
+done
