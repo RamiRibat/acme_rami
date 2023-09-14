106b416d3c9f65b8e625673e003099ed4412d27c
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-14 04:33:35 -0400

    update x32

---
+++ next/rl_continuous/script_local.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TASK_SUITES=(
+    # 'gym'
+    'control'
+    # 'atari26'
+    # 'atari52'
+    # 'atari57'
+)
+
+
+DATETIME=$(date +'%Y%m%d:%H%M%S')
+
+ID="local_"$DATETIME
+
+SEEDS=(1 2 3)
+
+
+source ~/.bashrc
+
+conda activate acme
+
+for SUITE in ${TASK_SUITES[*]}; do
+    for s in ${!SEEDS[*]}; do
+        python run_d4pg.py --acme_id $ID --suite $SUITE --seed ${SEEDS[s]}
+    done
+done
+
+conda deactivate
