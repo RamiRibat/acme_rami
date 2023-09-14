76412b09c6061b48118013e3a47e5bfceb43e2b4
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-14 02:14:56 -0400

    update

--- next/rl_continuous/script.sh
+++ next/rl_continuous/script.sh
@@ -21,8 +21,15 @@ source ~/.bashrc
 
 conda activate acme
 
+# for SUITE in ${TASK_SUITES[*]}; do
+#     for s in ${!SEEDS[*]}; do
+#         CUDA_VISIBLE_DEVICES=${GPUS[s]} python run_d4pg.py --acme_id $ID --suite $SUITE --seed ${SEEDS[s]} &
+#     done
+# done
+
 for SUITE in ${TASK_SUITES[*]}; do
-    for s in ${!SEEDS[*]}; do
-        CUDA_VISIBLE_DEVICES=${GPUS[s]} python run_d4pg.py --acme_id $ID --suite $SUITE --seed ${SEEDS[s]} &
-    done
+    echo "SEED " $1 >> ~/logdir/vector.log
+    # python run_d4pg.py --acme_id $ID --suite $SUITE --seed $1
 done
+
+conda deactivate
