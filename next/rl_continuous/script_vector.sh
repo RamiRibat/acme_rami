750527cbe151c9a2f1fba76cdc6b79124e34a836
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-14 05:56:53 -0400

    update

--- next/rl_continuous/script_vector.sh
+++ next/rl_continuous/script_vector.sh
@@ -4,8 +4,12 @@ source ~/.bashrc
 
 conda activate acme
 
-# echo "SEED " $3 >> ~/logdir/vector.log
-# echo "CUDA " $CUDA_VISIBLE_DEVICES >> ~/logdir/vector.log
-python run_d4pg.py --acme_id $1 --suite $2 --seed $3
+if [ $3 == 1 ]; then
+    sleep 30
+fi
+
+echo "SEED " $3 >> ~/logdir/vector.log
+echo "CUDA " $CUDA_VISIBLE_DEVICES >> ~/logdir/vector.log
+# python run_d4pg.py --acme_id $1 --suite $2 --seed $3
 
 conda deactivate
