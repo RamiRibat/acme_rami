858fd3200c853b06b753c8994ed26fd8a4a8358f
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-14 03:29:05 -0400

    update

--- next/rl_continuous/script_vector.sh
+++ next/rl_continuous/script_vector.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-TASK_SUITES=(
-    # 'gym'
-    'control'
-    # 'atari26'
-    # 'atari52'
-    # 'atari57'
-)
-
-
-DATETIME=$(date +'%Y%m%d:%H%M%S')
-
-ID="vector_"$DATETIME
-
 source ~/.bashrc
 
 conda activate acme
 
-# echo "SEED " $3 >> ~/logdir/vector.log
-# echo "CUDA " $CUDA_VISIBLE_DEVICES >> ~/logdir/vector.log
-python run_d4pg.py --acme_id $1 --suite $2 --seed $3
+echo "SEED " $3 >> ~/logdir/vector.log
+echo "CUDA " $CUDA_VISIBLE_DEVICES >> ~/logdir/vector.log
+# python run_d4pg.py --acme_id $1 --suite $2 --seed $3
 
 conda deactivate
