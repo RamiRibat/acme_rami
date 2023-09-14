6121d3fedb3b490a047ce6873c4f61f746eaf7d0
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-13 22:48:02 -0400

    update

--- next/rl_continuous/script_vector.sh
+++ next/rl_continuous/script_vector.sh
@@ -9,11 +9,7 @@
 
 #SBATCH --cpus-per-task=24
 
-#SBATCH --mem-per-cpu=24G
-
-#SBATCH --output=slurm-%j.out
-
-#SBATCH --error=slurm-%j.err
+#SBATCH --mem-per-cpu=1G
 
 # prepare your environment here
 module load cuda-11.8
