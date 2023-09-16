3596ca6a75482d2e6611a2c96c48f2740563d03e
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-16 02:03:37 -0400

    update

--- next/rl_continuous/script_vector_parallel.sh
+++ next/rl_continuous/script_vector_parallel.sh
@@ -25,11 +25,13 @@ for SUITE in ${SUITES[*]}; do
     for LEVEL in ${LEVELS[*]}; do
         # for TASK in ${TASKS[*]}; do
         # echo "SUITE: " $SUITE "LEVEL: " $LEVEL
-        python run_d4pg_v.py --acme_id $1 --seed $2 --suite $SUITE --level $LEVEL
+        python run_d4pg_v.py --acme_id $1 --seed $2 --suite $SUITE --level $LEVEL &
         # python run_d4pg_v.py --acme_id 'v_20230914:185741' --seed 0 --suite 'control' --level 'easy'
         # python run_ppo.py --acme_id $1 --seed $2 --suite $SUITE --level $LEVEL
         # done
     done
 done
+
+wait
 
 conda deactivate
