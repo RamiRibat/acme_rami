30a661ee9afbb83fe2aff9a836745e813659b819
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-19 17:50:22 -0400

    update scripts

--- next/rl_continuous/script_local.sh
+++ next/rl_continuous/script_local.sh
@@ -8,7 +8,7 @@ SUITES=(
 
 LEVELS=(
     'trivial'
-    'easy'
+    # 'easy'
     # 'medium'
     # 'hard'
 )
@@ -16,10 +16,10 @@ LEVELS=(
 
 DATETIME=$(date +'%Y%m%d:%H%M%S')
 
-# ID="local_next_"$DATETIME
-ID="local_next_20230918:045122"
+# ID="local_"$DATETIME
+ID="local_test_acme"
 
-SEEDS=(1 2 3)
+SEEDS=(1)
 
 
 source ~/.bashrc
@@ -29,11 +29,12 @@ conda activate acme
 for SEED in ${SEEDS[*]}; do
     for SUITE in ${SUITES[*]}; do
         for LEVEL in ${LEVELS[*]}; do
+            MUJOCO_GL=egl python run_ppo.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+            # MUJOCO_GL=egl python run_sac.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
             # MUJOCO_GL=egl python run_d4pg.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
-            # MUJOCO_GL=egl python run_d4pg.py --acme_id 'test_d4pg' --seed 0 --suite 'control' --level 'easy'
-            MUJOCO_GL=egl python run_d4pg_next.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+            # MUJOCO_GL=egl python run_d4pg_next.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
         done
     done
 done 
 
-conda deactivate
+# conda deactivate
---
+++ next/vector/script_vector.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+conda activate acme
+
+SUITES=(
+    # 'gym'
+    'control'
+    # 'atari'
+)
+
+LEVELS=(
+    'trivial'
+    'easy'
+    'medium'
+    'hard'
+)
+
+ID=$1
+AGENT=$2
+SEED=$2
+
+for SUITE in ${SUITES[*]}; do
+    for LEVEL in ${LEVELS[*]}; do
+        MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+    done
+done
+
+conda deactivate
