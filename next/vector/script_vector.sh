4eee4d73b148430076762411a6b410aed0afbc2a
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-19 19:09:27 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -23,7 +23,20 @@ SEED=$3
 
 for SUITE in ${SUITES[*]}; do
     for LEVEL in ${LEVELS[*]}; do
-        MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+        # MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+        # MUJOCO_GL=egl \
+        # XLA_PYTHON_CLIENT_MEM_FRACTION=0.5 \
+        # python ../rl_continuous/run_$AGENT.py \
+        # --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+        MUJOCO_GL=egl \
+        XLA_PYTHON_CLIENT_MEM_FRACTION=0.5 \
+        python ../rl_continuous/run_ppo.py \
+        --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
+        MUJOCO_GL=egl \
+        XLA_PYTHON_CLIENT_MEM_FRACTION=0.5 \
+        python ../rl_continuous/run_sac.py \
+        --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
+        wait
     done
 done
 
