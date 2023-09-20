f87bf76d06c3fcac6e270abcb6c67bfac173bb2d
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-19 20:17:32 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -31,7 +31,7 @@ for SUITE in ${SUITES[*]}; do
             # MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
 
             MUJOCO_GL=egl \
-            XLA_PYTHON_CLIENT_MEM_FRACTION=MEM_FRACTION \
+            XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
             python ../rl_continuous/run_$AGENT.py \
             --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL &
 
