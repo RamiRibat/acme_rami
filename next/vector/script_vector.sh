3c45351f26256f870c57467c815fa50cc40c9fef
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-19 20:33:39 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -23,16 +23,18 @@ AGENT=$2
 # SEED=$3
 SEEDS=(1 2 3)
 # MEM_FRACTION="0.75/${#SEEDS[@]}" | bc -l
-MEM_FRACTION=$( echo "0.75/${#SEEDS[@]}" | bc -l )
-echo "MEM_FRACTION " $MEM_FRACTION
+FRACTION=$( echo "0.75/${#SEEDS[@]}" | bc -l )
+MEM_FRACTION=FRACTION
 
 for SUITE in ${SUITES[*]}; do
     for LEVEL in ${LEVELS[*]}; do
         for SEED in ${SEEDS[*]}; do
+            echo "MEM_FRACTION " $MEM_FRACTION
+            
             # MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
 
             MUJOCO_GL=egl \
-            XLA_PYTHON_CLIENT_MEM_FRACTION=eval($MEM_FRACTION) \
+            XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
             python ../rl_continuous/run_$AGENT.py \
             --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL &
 
