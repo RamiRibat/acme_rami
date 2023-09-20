f5a7050bba1cf7b1bfbdb752fee25b9a4d65276a
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-19 21:04:29 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -43,7 +43,7 @@ for SUITE in ${SUITES[*]}; do
         MUJOCO_GL=egl \
         XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
         python ../rl_continuous/run_sac.py \
-        --acme_id 'v_test' --seed 2 --suite 'control' --level 'trivial' &
+        --acme_id 'v_test' --seed 1 --suite 'control' --level 'trivial' &
 
         MUJOCO_GL=egl \
         XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
