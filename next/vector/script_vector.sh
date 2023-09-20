f9de0b1bd160cbcfc111577111442a7784a6a45b
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-19 20:45:20 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -21,38 +21,36 @@ ID=$1
 AGENT=$2
 
 # SEED=$3
-SEEDS=(1 2 3)
-# MEM_FRACTION="0.75/${#SEEDS[@]}" | bc -l
-# FRACTION=$( echo "75/${#SEEDS[@]}" | bc -l )
-# FRACTION=$(("75/${#SEEDS[@]}"))
-MEM_FRACTION=0.$(("75/${#SEEDS[@]}"))
+SEEDS=(1)
+MEM_PERCENTAGE=$(("75/${#SEEDS[@]}"))
+MEM_FRACTION=0.$MEM_PERCENTAGE
 
 for SUITE in ${SUITES[*]}; do
     for LEVEL in ${LEVELS[*]}; do
         for SEED in ${SEEDS[*]}; do
-            echo "MEM_FRACTION " $MEM_FRACTION
+            # echo "MEM_FRACTION " $MEM_FRACTION
 
             # MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
 
+            # MUJOCO_GL=egl \
+            # XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
+            # python ../rl_continuous/run_$AGENT.py \
+            # --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL &
+
             MUJOCO_GL=egl \
             XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
-            python ../rl_continuous/run_$AGENT.py \
-            --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL &
+            python ../rl_continuous/run_ppo.py \
+            --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
 
-            # # MUJOCO_GL=egl \
-            # # # XLA_PYTHON_CLIENT_MEM_FRACTION=0.45 \
-            # # python ../rl_continuous/run_ppo.py \
-            # # --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
+            MUJOCO_GL=egl \
+            XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
+            python ../rl_continuous/run_sac.py \
+            --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
 
-            # MUJOCO_GL=egl \
-            # XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 \
-            # python ../rl_continuous/run_sac.py \
-            # --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
-
-            # MUJOCO_GL=egl \
-            # XLA_PYTHON_CLIENT_MEM_FRACTION=0.4 \
-            # python ../rl_continuous/run_d4pg.py \
-            # --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial'
+            MUJOCO_GL=egl \
+            XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
+            python ../rl_continuous/run_d4pg.py \
+            --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial'
         done
     done
 done
