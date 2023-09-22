ae9f1eba860e2ae8c7cef27d255e878ee7f73ce3
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-22 02:03:53 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -25,31 +25,10 @@ MEM_FRACTION=0.8
 
 for SUITE in ${SUITES[*]}; do
     for LEVEL in ${LEVELS[*]}; do
-        # for SEED in ${SEEDS[*]}; do
-        # XLA_PYTHON_CLIENT_MEM_FRACTION=false
-
-        # MUJOCO_GL=egl python ../rl_continuous/run_$AGENT.py --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
-
         MUJOCO_GL=egl \
         XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
         python ../rl_continuous/run_$AGENT.py \
         --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
-
-        # MUJOCO_GL=egl \
-        # XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
-        # python ../rl_continuous/run_ppo.py \
-        # --acme_id 'v_test' --seed 0 --suite 'control' --level 'trivial' &
-
-        # MUJOCO_GL=egl \
-        # XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
-        # python ../rl_continuous/run_sac.py \
-        # --acme_id 'v_test' --seed 1 --suite 'control' --level 'trivial' &
-
-        # MUJOCO_GL=egl \
-        # XLA_PYTHON_CLIENT_MEM_FRACTION=0.25 \
-        # python ../rl_continuous/run_sac.py \
-        # --acme_id 'v_test' --seed 2 --suite 'control' --level 'trivial'
-        # done
     done
 done
 
