803a3377553d1359116222349fc347085b4d8b2a
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-22 02:53:06 -0400

    update

--- next/vector/script_vector.sh
+++ next/vector/script_vector.sh
@@ -11,15 +11,16 @@ SUITES=(
 )
 
 LEVELS=(
-    # 'trivial'
-    # 'easy'
-    # 'medium'
+    'trivial'
+    'easy'
+    'medium'
     'hard'
 )
 
 ID=$1
 AGENT=$2
-SEED=$3
+SUFFIX=$3
+SEED=$4
 
 MEM_FRACTION=0.8
 
@@ -28,7 +29,7 @@ for SUITE in ${SUITES[*]}; do
         MUJOCO_GL=egl \
         XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
         python ../rl_continuous/run_$AGENT.py \
-        --acme_id $ID --seed $SEED --suite $SUITE --level $LEVEL
+        --acme_id $ID --agent_id $AGENT"_"$SUFFIX --seed $SEED --suite $SUITE --level $LEVEL
     done
 done
 
