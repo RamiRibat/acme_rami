d91e6be83fd71423108270c7ede2e6ce3c94301f
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-23 06:44:15 -0400

    add vector ppo

---
+++ next/vector/script_vector_ppo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+conda activate acme
+
+SUITES=(
+    # 'gym'
+    'control'
+    # 'atari'
+)
+
+LEVELS=(
+    'trivial'
+    'easy'
+    'medium'
+    'hard'
+)
+
+ID=$1
+AGENT=$2
+HP=$3
+SEED=$4
+
+MEM_FRACTION=0.75
+
+for SUITE in ${SUITES[*]}; do
+    for LEVEL in ${LEVELS[*]}; do
+        MUJOCO_GL=egl \
+        XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
+        python ../rl_continuous/run_$AGENT.py \
+        --acme_id $ID --agent_id $AGENT"_hp_"$HP --replay_ratio $RR --seed $SEED --suite $SUITE --level $LEVEL
+    done
+done
+
+conda deactivate
