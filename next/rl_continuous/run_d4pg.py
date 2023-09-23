b335728f592a8d0a882f4d49c5273a8827904956
Rami Ribat <Rami.Ribat@gmail.com>
2023-09-23 01:36:38 -0400

    update d4pg

--- next/rl_continuous/run_d4pg.py
+++ next/rl_continuous/run_d4pg.py
@@ -77,8 +77,8 @@ def build_experiment_config():
 	vmax = vmax_values[suite]
 
 	replay_ratio = eval(FLAGS.replay_ratio)
-	d4pg_hyperparams['samples_per_insert'] *= (replay_ratio * d4pg_hyperparams['batch_size'])
-	d4pg_hyperparams['num_sgd_steps_per_step'] *= (replay_ratio * (d4pg_hyperparams['batch_size']/d4pg_hyperparams['samples_per_insert']))
+	d4pg_hyperparams['samples_per_insert'] *= int((replay_ratio * d4pg_hyperparams['batch_size']))
+	d4pg_hyperparams['num_sgd_steps_per_step'] *= int((replay_ratio * (d4pg_hyperparams['batch_size']/d4pg_hyperparams['samples_per_insert'])))
 
 	def network_factory(spec) -> d4pg.D4PGNetworks:
 		return d4pg.make_networks(
