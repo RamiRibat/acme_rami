fc7c8870a4bde441e57a99f9b2a1537336c7faf3
Rami Ribat <Rami.Ribat@gmail.com>
2023-10-09 10:08:05 -0400

    update

---
+++ next/scripts_vector/script_vector_sac.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+conda activate acme
+
+SUITES=(
+    # 'gym'
+    # 'control'
+    'dmc'
+    # 'atari'
+)
+
+LEVELS=(
+    'trivial'
+    'easy'
+    'medium'
+    'hard'
+    # 'extra'
+)
+
+ID=$1
+AGENT=$2
+HP=$3
+SEED=$4
+
+MEM_FRACTION=0.75
+
+for SUITE in ${SUITES[*]}; do
+    for LEVEL in ${LEVELS[*]}; do
+        MUJOCO_GL=egl \
+        XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
+        python ../rl_continuous/run_$AGENT.py \
+        --acme_id $ID --agent_id $AGENT --seed $SEED --suite $SUITE --level $LEVEL
+    done
+done
+
+conda deactivate
