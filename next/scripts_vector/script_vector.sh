7c3db87756a2ded84af59aa292513a4b425af315
Rami Ribat <Rami.Ribat@gmail.com>
2023-10-09 22:38:50 -0400

    update

--- next/scripts_vector/script_vector.sh
+++ next/scripts_vector/script_vector.sh
@@ -5,18 +5,18 @@ source ~/.bashrc
 conda activate acme
 
 SUITES=(
-    # 'gym'
-    # 'control'
-    'dmc'
-    # 'atari'
+	# 'gym'
+	# 'control'
+	'dmc'
+	# 'atari'
 )
 
 DMC_LEVELS=(
-    'trivial'
-    'easy'
-    'medium'
-    'hard'
-    # 'extra'
+	'trivial'
+	'easy'
+	'medium'
+	'hard'
+	# 'extra'
 )
 
 ID=$1
@@ -27,25 +27,28 @@ SEED=$4
 MEM_FRACTION=0.75
 
 for SUITE in ${SUITES[*]}; do
-    if [ $SUITE == 'gym' ]; then
-        echo $SUITE
-        MUJOCO_GL=egl \
-            XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
-            python run_$AGENT.py \
-            --acme_id $ID --agent_id $AGENT --hp $HP \
-            --suite $SUITE --level $LEVEL \
-            --seed $SEED
-    fi
-    if [ $SUITE == 'control' ] || [ $SUITE == 'dmc' ]; then
-        echo $SUITE
-        for LEVEL in ${DMC_LEVELS[*]}; do
-            MUJOCO_GL=egl \
-            XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
-            python ../rl_continuous/run_$AGENT.py \
-            --acme_id $ID --agent_id $AGENT --hp $HP \
-            --suite $SUITE --level $LEVEL \
-            --seed $SEED
-    fi
+
+	if [ $SUITE == 'gym' ]; then
+		echo $SUITE
+		MUJOCO_GL=egl \
+			XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
+			python run_$AGENT.py \
+			--acme_id $ID --agent_id $AGENT --hp $HP \
+			--suite $SUITE --level $LEVEL \
+			--seed $SEED
+	fi
+	
+	if [ $SUITE == 'control' ] || [ $SUITE == 'dmc' ]; then
+		echo $SUITE
+		for LEVEL in ${DMC_LEVELS[*]}; do
+			MUJOCO_GL=egl \
+			XLA_PYTHON_CLIENT_MEM_FRACTION=$MEM_FRACTION \
+			python ../rl_continuous/run_$AGENT.py \
+			--acme_id $ID --agent_id $AGENT --hp $HP \
+			--suite $SUITE --level $LEVEL \
+			--seed $SEED
+	fi
+
 done
 
 conda deactivate
