68f8a46c946f2c321df4010c778aacd369eb34db
Rami Ribat <Rami.Ribat@gmail.com>
2023-10-10 00:53:39 -0400

    update loggers

--- acme/utils/loggers/csv.py
+++ acme/utils/loggers/csv.py
@@ -29,115 +29,134 @@ from acme.utils.loggers import base
 
 
 class CSVLogger(base.Logger):
-  """Standard CSV logger.
+	"""Standard CSV logger.
 
-  The fields are inferred from the first call to write() and any additional
-  fields afterwards are ignored.
+	The fields are inferred from the first call to write() and any additional
+	fields afterwards are ignored.
 
-  TODO(jaslanides): Consider making this stateless/robust to preemption.
-  """
+	TODO(jaslanides): Consider making this stateless/robust to preemption.
+	"""
 
-  _open = open
+	_open = open
 
-  def __init__(
-      self,
-      directory_or_file: Union[str, TextIO] = '~/logdir/acme',
-      label: str = '',
-      time_delta: float = 0.,
-      add_uid: bool = True,
-      flush_every: int = 30,
-  ):
-    """Instantiates the logger.
+	def __init__(
+		self,
+		directory_or_file: Union[str, TextIO] = '~/logdir/acme',
+		label: str = '',
+		time_delta: float = 0.,
+		add_uid: bool = True,
+		flush_every: int = 30,
+	):
+		"""Instantiates the logger.
 
-    Args:
-      directory_or_file: Either a directory path as a string, or a file TextIO
-        object.
-      label: Extra label to add to logger. This is added as a suffix to the
-        directory.
-      time_delta: Interval in seconds between which writes are dropped to
-        throttle throughput.
-      add_uid: Whether to add a UID to the file path. See `paths.process_path`
-        for details.
-      flush_every: Interval (in writes) between flushes.
-    """
+		Args:
+			directory_or_file: Either a directory path as a string, or a file TextIO
+			object.
+			label: Extra label to add to logger. This is added as a suffix to the
+			directory.
+			time_delta: Interval in seconds between which writes are dropped to
+			throttle throughput.
+			add_uid: Whether to add a UID to the file path. See `paths.process_path`
+			for details.
+			flush_every: Interval (in writes) between flushes.
+		"""
 
-    if flush_every <= 0:
-      raise ValueError(
-          f'`flush_every` must be a positive integer (got {flush_every}).')
+		if flush_every <= 0:
+			raise ValueError(
+				f'`flush_every` must be a positive integer (got {flush_every}).')
 
-    self._last_log_time = time.time() - time_delta
-    self._time_delta = time_delta
-    self._flush_every = flush_every
-    self._add_uid = add_uid
-    self._writer = None
-    self._file_owner = False
-    self._file = self._create_file(directory_or_file, label)
-    self._writes = 0
-    logging.info('Logging to %s', self.file_path)
+		self._last_log_time = time.time() - time_delta
 
-  def _create_file(
-      self,
-      directory_or_file: Union[str, TextIO],
-      label: str,
-  ) -> TextIO:
-    """Opens a file if input is a directory or use existing file."""
-    if isinstance(directory_or_file, str):
-      # directory = paths.process_path(
-      #     directory_or_file, 'logs', label, add_uid=self._add_uid)
-      directory = paths.process_path(
-          directory_or_file, label, add_uid=self._add_uid)
-      file_path = os.path.join(directory, 'logs.csv')
-      self._file_owner = True
-      return self._open(file_path, mode='a')
+		self._label = label
+		if label == 'evaluator':
+			self._time_delta = 0.
+			self._flush_every = 1
+		else:
+			self._time_delta = time_delta
+			self._flush_every = flush_every
 
-    # TextIO instance.
-    file = directory_or_file
-    if label:
-      logging.info('File, not directory, passed to CSVLogger; label not used.')
-    if not file.mode.startswith('a'):
-      raise ValueError('File must be open in append mode; instead got '
-                       f'mode="{file.mode}".')
-    return file
+		self._add_uid = add_uid
+		self._writer = None
+		self._file_owner = False
+		self._file = self._create_file(directory_or_file, label)
+		self._writes = 0
 
-  def write(self, data: base.LoggingData):
-    """Writes a `data` into a row of comma-separated values."""
-    # Only log if `time_delta` seconds have passed since last logging event.
-    now = time.time()
+		logging.info('Logging to %s', self.file_path)
 
-    # TODO(b/192227744): Remove this in favour of filters.TimeFilter.
-    elapsed = now - self._last_log_time
-    if elapsed < self._time_delta:
-      logging.debug('Not due to log for another %.2f seconds, dropping data.',
-                    self._time_delta - elapsed)
-      return
-    self._last_log_time = now
 
-    # Append row to CSV.
-    data = base.to_numpy(data)
-    # Use fields from initial `data` to create the header. If extra fields are
-    # present in subsequent `data`, we ignore them.
-    if not self._writer:
-      fields = sorted(data.keys())
-      self._writer = csv.DictWriter(self._file, fieldnames=fields,
-                                    extrasaction='ignore')
-      # Write header only if the file is empty.
-      if not self._file.tell():
-        self._writer.writeheader()
-    self._writer.writerow(data)
+	def _create_file(
+		self,
+		directory_or_file: Union[str, TextIO],
+		label: str,
+	) -> TextIO:
+		"""Opens a file if input is a directory or use existing file."""
+		if isinstance(directory_or_file, str):
+			# directory = paths.process_path(
+			#     directory_or_file, 'logs', label, add_uid=self._add_uid)
+			directory = paths.process_path(
+				directory_or_file, label, add_uid=self._add_uid)
+			file_path = os.path.join(directory, 'logs.csv')
+			self._file_owner = True
+			# return self._open(file_path, mode='a')
+			return self._open(file_path, mode='w')
 
-    # Flush every `flush_every` writes.
-    if self._writes % self._flush_every == 0:
-      self.flush()
-    self._writes += 1
+		# TextIO instance.
+		file = directory_or_file
+		if label:
+			logging.info('File, not directory, passed to CSVLogger; label not used.')
+		if not file.mode.startswith('a'):
+			raise ValueError('File must be open in append mode; instead got '
+							f'mode="{file.mode}".')
+	
+		return file
 
-  def close(self):
-    self.flush()
-    if self._file_owner:
-      self._file.close()
 
-  def flush(self):
-    self._file.flush()
+	def write(self, data: base.LoggingData):
+		"""Writes a `data` into a row of comma-separated values."""
+		# Only log if `time_delta` seconds have passed since last logging event.
+		now = time.time()
 
-  @property
-  def file_path(self) -> str:
-    return self._file.name
+		# TODO(b/192227744): Remove this in favour of filters.TimeFilter.
+		elapsed = now - self._last_log_time
+		
+		if self._label == 'evaluator':
+			pass
+		else:
+			if elapsed < self._time_delta:
+				logging.debug('Not due to log for another %.2f seconds, dropping data.',
+								self._time_delta - elapsed)
+				return
+		
+		self._last_log_time = now
+
+		# Append row to CSV.
+		data = base.to_numpy(data)
+		# Use fields from initial `data` to create the header. If extra fields are
+		# present in subsequent `data`, we ignore them.
+		if not self._writer:
+			fields = sorted(data.keys())
+			self._writer = csv.DictWriter(self._file, fieldnames=fields, extrasaction='ignore')
+			# Write header only if the file is empty.
+			if not self._file.tell(): self._writer.writeheader()
+
+		self._writer.writerow(data)
+
+		# Flush every `flush_every` writes.
+		if self._writes % self._flush_every == 0: self.flush()
+
+		self._writes += 1
+
+
+	def close(self):
+		self.flush()
+		if self._file_owner:
+			self._file.close()
+
+
+	def flush(self):
+		self._file.flush()
+
+
+	@property
+	def file_path(self) -> str:
+		return self._file.name
--- acme/utils/loggers/default.py
+++ acme/utils/loggers/default.py
@@ -26,45 +26,52 @@ from acme.utils.loggers import terminal
 
 
 def make_default_logger(
-    label: str,
-    save_data: bool = True,
-    time_delta: float = 1.0,
-    asynchronous: bool = False,
-    print_fn: Optional[Callable[[str], None]] = None,
-    serialize_fn: Optional[Callable[[Mapping[str, Any]], str]] = base.to_numpy,
-    steps_key: str = 'steps',
+	label: str,
+	save_data: bool = True,
+	time_delta: float = 1.0,
+	asynchronous: bool = False,
+	print_fn: Optional[Callable[[str], None]] = None,
+	serialize_fn: Optional[Callable[[Mapping[str, Any]], str]] = base.to_numpy,
+	steps_key: str = 'steps',
 ) -> base.Logger:
-  """Makes a default Acme logger.
+	"""Makes a default Acme logger.
 
-  Args:
-    label: Name to give to the logger.
-    save_data: Whether to persist data.
-    time_delta: Time (in seconds) between logging events.
-    asynchronous: Whether the write function should block or not.
-    print_fn: How to print to terminal (defaults to print).
-    serialize_fn: An optional function to apply to the write inputs before
-      passing them to the various loggers.
-    steps_key: Ignored.
+	Args:
+	label: Name to give to the logger.
+	save_data: Whether to persist data.
+	time_delta: Time (in seconds) between logging events.
+	asynchronous: Whether the write function should block or not.
+	print_fn: How to print to terminal (defaults to print).
+	serialize_fn: An optional function to apply to the write inputs before
+		passing them to the various loggers.
+	steps_key: Ignored.
 
-  Returns:
-    A logger object that responds to logger.write(some_dict).
-  """
-  del steps_key
-  if not print_fn:
-    print_fn = logging.info
-  terminal_logger = terminal.TerminalLogger(label=label, print_fn=print_fn)
+	Returns:
+	A logger object that responds to logger.write(some_dict).
+	"""
+   
+	del steps_key
+    
+	if not print_fn:
+		print_fn = logging.info
+    
+	terminal_logger = terminal.TerminalLogger(label=label, print_fn=print_fn)
 
-  loggers = [terminal_logger]
-  # loggers = []
+	loggers = [terminal_logger]
+	# loggers = []
 
-  if save_data:
-    loggers.append(csv.CSVLogger(label=label))
+	if save_data:
+		loggers.append(csv.CSVLogger(label=label))
 
-  # Dispatch to all writers and filter Nones and by time.
-  logger = aggregators.Dispatcher(loggers, serialize_fn)
-  logger = filters.NoneFilter(logger)
-  if asynchronous:
-    logger = async_logger.AsyncLogger(logger)
-  logger = filters.TimeFilter(logger, time_delta)
+	# Dispatch to all writers and filter Nones and by time.
+	logger = aggregators.Dispatcher(loggers, serialize_fn)
 
-  return logger
+	logger = filters.NoneFilter(logger)
+
+	if asynchronous:
+		logger = async_logger.AsyncLogger(logger)
+
+	if label != 'evaluator':
+		logger = filters.TimeFilter(logger, time_delta)
+
+	return logger
