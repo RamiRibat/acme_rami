51cbeb1cd9a5d2dffe511c082be113dd56f7f9c5
Rami Ribat <Rami.Ribat@gmail.com>
2023-10-17 15:52:21 -0400

    update impala

--- acme/agents/jax/impala/networks.py
+++ acme/agents/jax/impala/networks.py
@@ -14,17 +14,51 @@
 
 """IMPALA networks definition."""
 
-from acme import specs
+# Python
+# import dataclasses, functools, time
+# from typing import Any, Callable, Dict, Iterator, List, NamedTuple, Optional, Sequence, Tuple, Union
+
+# ML/DL
+import jax, chex, optax, rlax
+import jax.numpy as jnp
+import haiku as hk
+import haiku.initializers as hk_init
+import numpy as np
+import tensorflow_probability.substrates.jax as tfp
+import reverb
+from reverb import rate_limiters
+
+# ACME
+from acme import specs, types
+from acme.agents.jax import actor_core as actor_core_lib
 from acme.jax import networks as networks_lib
+from acme.jax import utils
+from acme.agents.jax.impala import config as impala_config
+
 
 
 IMPALANetworks = networks_lib.UnrollableNetwork
 
 
-def make_atari_networks(env_spec: specs.EnvironmentSpec) -> IMPALANetworks:
-  """Builds default IMPALA networks for Atari games."""
+def make_atari_networks(
+	spec: specs.EnvironmentSpec,
+	config: impala_config.IMPALAConfig,
+) -> IMPALANetworks:
+	"""Builds default IMPALA networks for Atari games."""
 
-  def make_core_module() -> networks_lib.DeepIMPALAAtariNetwork:
-    return networks_lib.DeepIMPALAAtariNetwork(env_spec.actions.num_values)
+	def make_core_module() -> networks_lib.DeepIMPALAAtariNetwork:
+		return networks_lib.DeepIMPALAAtariNetwork(spec.actions.num_values)
 
-  return networks_lib.make_unrollable_network(env_spec, make_core_module)
+	return networks_lib.make_unrollable_network(spec, make_core_module)
+
+
+def make_networks(
+	spec: specs.EnvironmentSpec,
+	config: impala_config.IMPALAConfig,
+) -> IMPALANetworks:
+	"""Builds default IMPALA networks for Atari games."""
+
+	def make_core_module() -> networks_lib.DeepIMPALAAtariNetwork:
+		return networks_lib.DeepIMPALAAtariNetwork(spec.actions.num_values)
+
+	return networks_lib.make_unrollable_network(spec, make_core_module)
