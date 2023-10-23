907d6eb8f142846080c20fd7947baa6c91a2e948
Rami Ribat <Rami.Ribat@gmail.com>
2023-10-23 03:50:43 -0400

    update impala

--- acme/agents/jax/impala/learning.py
+++ acme/agents/jax/impala/learning.py
@@ -145,7 +145,7 @@ class IMPALALearner(acme.Learner):
 
 	def _make_initial_state(self) -> TrainingState:
 		"""Initialises the training state (parameters and optimiser state)."""
-		print(colored('\nLearner._make_initial_state', 'red'))
+		# print(colored('Learner._make_initial_state', 'red'))
 
 		self._key, _ = jax.random.split(self._key)
 
@@ -188,5 +188,4 @@ class IMPALALearner(acme.Learner):
 
 
 	def restore(self, state: TrainingState):
-		print(colored('\nLearner.restore', 'red'))
 		self._state = utils.replicate_in_all_devices(state, self._local_devices)
