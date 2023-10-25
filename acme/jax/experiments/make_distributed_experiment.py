b5139fd5f6308de1a85b1393efcc5f2e25aeac17
Rami Ribat <Rami.Ribat@gmail.com>
2023-10-25 01:53:55 -0400

    update

--- acme/jax/experiments/make_distributed_experiment.py
+++ acme/jax/experiments/make_distributed_experiment.py
@@ -547,22 +547,22 @@ def make_distributed_experiment(
 
 	eval_dict = {}
 
-	# for evaluator in experiment.get_evaluator_factories():
-	# 	key, evaluator_key = jax.random.split(key)
-	# 	eval_loop = program.add_node(
-	# 		lp.CourierNode(
-	# 			evaluator,
-	# 			evaluator_key, # random_key
-	# 			learner, # variable_source
-	# 			counter, # parent counter
-	# 			experiment.builder.make_actor, # make_actor
-	# 		),
-	# 		label='evaluator'
-	# 	)
+	for evaluator in experiment.get_evaluator_factories():
+		key, evaluator_key = jax.random.split(key)
+		eval_loop = program.add_node(
+			lp.CourierNode(
+				evaluator,
+				evaluator_key, # random_key
+				learner, # variable_source
+				counter, # parent counter
+				experiment.builder.make_actor, # make_actor
+			),
+			label='evaluator'
+		)
 
-	# 	eval_dict['eval_loop'] = eval_loop
-	# 	eval_dict['eval_points'] = eval_points
-	# 	eval_dict['eval_episodes'] = eval_episodes
+		eval_dict['eval_loop'] = eval_loop
+		eval_dict['eval_points'] = eval_points
+		eval_dict['eval_episodes'] = eval_episodes
 
 
 
